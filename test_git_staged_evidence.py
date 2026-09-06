2ef747452724a03a53ecdd9160c35f83fb4b1071
badeemorse961-lang <badeemorse961@gmail.com>
2026-09-06 23:07:12 +0300

    Fix unterminated staged-index evidence test fixture

--- test_git_staged_evidence.py
+++ test_git_staged_evidence.py
@@ -30,7 +30,8 @@ def test_parse_rejects_line_oriented_output() -> None:
 def test_parse_rejects_unterminated_output() -> None:
     with pytest.raises(GitStagedEvidenceError, match="not terminated"):
         parse_nul_index_records(
-            "100644 abcdef0123456789abcdef0123456789abcdef01 0\tfile.py\0",
+            "100644 abcdef0123456789abcdef0123456789abcdef01 0\tfile.py\0"
+            "100644 0123456789abcdef0123456789abcdef01234567 0\tother.py",
             object_format="sha1",
         )
 
